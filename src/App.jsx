3b96cbcf1bfae623b320338c02daad96f4641bc0
dev-from-cbd <veryfatwombat@gmail.com>
2024-08-22 16:18:57 +0930

    App

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,51 @@ function App() {
     <>
       {showLoader && (
         <div className="preloader">
-          <svg viewBox="0 0 1000 1000" preserveAspectRatio="none"></svg>
+          <svg viewBox="0 0 1000 1000" preserveAspectRatio="none">
+            <path
+              id="preloaderSvg"
+              d="M0,1005S175,995,500,995s500,5,500,5V0H0Z"
+            ></path>
+          </svg>
+
+          <div className="preloader-heading">
+            <div className="load-text">
+              <span>L</span>
+              <span>o</span>
+              <span>a</span>
+              <span>d</span>
+              <span>i</span>
+              <span>n</span>
+              <span>g</span>
+            </div>
+          </div>
         </div>
       )}
-      <h1>H1</h1>
+
+      {!showLoader && (
+        <>
+          <BrowserRouter>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <Layout>
+                    <Home />
+                  </Layout>
+                }
+              />
+              <Route
+                path="/details"
+                element={
+                  <Layout>
+                    <PortfolioDetails />
+                  </Layout>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+        </>
+      )}
     </>
   );
 }
